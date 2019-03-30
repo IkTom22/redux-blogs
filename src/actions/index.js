0a2f88f7465ddd102f091fa8c5aedb70879ca6b8
Iko <ikotominaga@gmail.com>
2019-03-30 14:23:58 +1300

    redux thunk hooked with middleware and still need to refactor the code in action index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,17 @@
 import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 
-export const fetchPosts = async () => {
-    // BAD APPROACH!!
-    const response = await jsonPlaceHolder.get('/posts');
-    return {
-        type: 'FETCH_POSTS',
-        payload: response
-    };
+export const fetchPosts = () => {
+    return async function (dispatch, getState) {
+        const response = await jsonPlaceHolder.get('/posts');
+        dispatch({ type: 'FETCH_POSTS', payload: response })
+
+    }
 };
+
+// TOTTALY FINE
+// export const selectPost = () => {
+//     return {
+//         type: 'SELECT_POST'
+//     }
+// };
+
