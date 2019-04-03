c9f68b02b059b5b23169db41c793ed03a69c39f5
Iko <ikotominaga@gmail.com>
2019-04-04 11:08:03 +1300

    userHeader logic added into mapStateToProps with "ownProps"

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,8 +7,7 @@ class UserHeader extends React.Component {
         this.props.fetchUser(this.props.userId);
     }
     render() {
-        //find is a bilt in js function
-        const user = this.props.users.find((user) => user.id === this.props.userId);
+        const { user } = this.props;
         if (!user) {
             return null;
         }
@@ -16,9 +15,9 @@ class UserHeader extends React.Component {
     }
 
 }
-
-const mapStateToProps = (state) => {
-    return { users: state.users };
+//ownProps is the props that are about to be sent into USerHeader component
+const mapStateToProps = (state, ownProps) => {
+    return { user: state.users.find(user => user.id === ownProps.userId) };
 }
 export default connect(
     mapStateToProps,
